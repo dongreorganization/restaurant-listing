7f8f70599ca0821576247f1b82248d82ca64c513
Dongre Jaipal <dongrejaipal1996@outlook.com>
2025-02-09 20:10:28 +0530

    Test Classed Added

--- src/test/java/com/dongrejaipal/restaurantlisting/RestaurantlistingApplicationTests.java
+++ src/test/java/com/dongrejaipal/restaurantlisting/RestaurantlistingApplicationTests.java
@@ -1,13 +1,8 @@
 package com.dongrejaipal.restaurantlisting;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestaurantlistingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
